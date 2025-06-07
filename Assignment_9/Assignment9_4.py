b766e5c02fff377dcf8451d7d6c07711ac3cd89b
prpatil7667 <prshntpatil051@gmail.com>
2025-06-07 11:52:12 +0530

    updated assignment9_4

--- Assignment_9/Assignment9_4.py
+++ Assignment_9/Assignment9_4.py
@@ -12,8 +12,19 @@ def main():
 
     time_start1 = time.time()
     print(sumof1toN(1000000))
+    time_end1 = time.time()
+    print(f"time took for normal call {time_end1 - time_start1}")
+
     time_start2 = time.time()
-    print(f"time took for normal call {time_start2 - time_start1}")
+    threading.Thread(target=sumof1toN, args=(1000000,)).start()
+    time_end2 = time.time()
+    print(f"time took for threading call {time_end2 - time_start2}")
+
+    time_start3 = time.time()
+    multiprocessing.Process(target=sumof1toN, args=(1000000,)).start()
+    time_end3 = time.time()
+    print(f"time took for multiprocessing call {time_end3 - time_start3}")
+
 
 if __name__ == "__main__":
     main()
